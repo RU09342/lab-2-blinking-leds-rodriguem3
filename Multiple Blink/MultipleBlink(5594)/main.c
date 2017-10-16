d846ddbcb29e22b02cbd7b5e5787552e54b2001f
rodriguem3 <31865089+rodriguem3@users.noreply.github.com>
2017-10-16 19:13:51 -0400

    Update main.c

--- Multiple Blink/MultipleBlink(5594)/main.c
+++ Multiple Blink/MultipleBlink(5594)/main.c
@@ -9,12 +9,10 @@ int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
 
-    // Configure GPIO
-    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
-    P1DIR |= BIT0;                          // Set P1.0 to output direction
+    P1OUT &= ~BIT0;                         // Clear P1.0 output
+    P1DIR |= BIT0;                          // Set P1.0 to output 
     P1OUT &= ~BIT1;                        // Clear P1.1 output latch
     P1DIR |= BIT1;                          // Set p1.1 output latch
-
     P5DIR &= ~BIT6;
     P5REN |= BIT6;
     P5OUT |= BIT6;
@@ -26,8 +24,8 @@ int main(void)
     {
         P1OUT ^= BIT0;                      // Toggle LED
         P1OUT ^= BIT1;                      //Toggle LED
-        __delay_cycles(500000);             //Adjust speed
-        P1OUT ^= BIT1;                      //Toggle one of the two LED at different rate
-        __delay_cycles(500000);             //Adjust speed
+        __delay_cycles(300000);             //delay
+        P1OUT ^= BIT1;                      //Toggle LED different rate
+        __delay_cycles(300000);             /delay
     }
 }
