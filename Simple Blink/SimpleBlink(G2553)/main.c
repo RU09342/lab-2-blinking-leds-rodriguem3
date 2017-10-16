6f7a4b6a4492e964b0ff99652bdcae9e61610c10
rodriguem3 <31865089+rodriguem3@users.noreply.github.com>
2017-10-16 19:11:35 -0400

    Update main.c

--- Simple Blink/SimpleBlink(G2553)/main.c
+++ Simple Blink/SimpleBlink(G2553)/main.c
@@ -3,27 +3,21 @@
 
 /**
  * main.c
- * Simple Blink (2553)
+ * Simple Blink (2311)
  */
 
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
-
-    // Configure GPIO
-    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
-    P1DIR |= BIT0;                          // Set P1.0 to output direction
-
-    //P5DIR &= ~BIT6;
-    //P5REN |= BIT6;
-   // P5OUT |= BIT6;
-
-   //PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
+    PM5CTL0 &= ~LOCKLPM5;    
+    P1DIR |= BIT0;                          // Set P1.0 output
+    P1OUT &= ~BIT0;                         // Clear P1.0 
 
     while(1)
     {
-        P1OUT ^= BIT0;                      // No Toggle LED
+        P1OUT ^= BIT0;                      //Toggle P1.0
         __delay_cycles(100000);
     }
 }
+
+
