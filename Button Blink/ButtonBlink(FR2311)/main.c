47fcdd70a82ba9c45522be14df10b6f8967117e7
rodriguem3 <31865089+rodriguem3@users.noreply.github.com>
2017-10-16 19:40:51 -0400

    Update main.c

--- Button Blink/ButtonBlink(FR2311)/main.c
+++ Button Blink/ButtonBlink(FR2311)/main.c
@@ -8,18 +8,15 @@
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
-
-    // Configure GPIO
-    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
-    P1DIR |= BIT0;                          // Set P1.0 to output direction
-    P2OUT &= ~BIT0;                        // Clear P1.1 output latch
-    P2DIR |= BIT0;                          // Set p1.1 output latch
-
+    P1DIR |= BIT0;                          // Set P1.0 to output 
+    P1OUT &= ~BIT0;                         // Clear P1.0 output 
+    P2DIR |= BIT0;                          // Set p1.1 output 
+    P2OUT &= ~BIT0;                        // Clear P1.1 output
     P1DIR &= ~BIT1;
     P1REN |= BIT1;
     P1OUT |= BIT1;
 
-    //PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
                                             // to activate previously configured port settings
     int swap = 1;
 
@@ -31,8 +28,8 @@ int main(void)
         P1OUT ^= BIT0;                      // Toggle LED
         __delay_cycles(500000);   //Adjust speed
         if(!(P1IN & BIT1)){
-                       swap = 0;
-                       P1OUT = ~BIT0;
+                 swap = 0;
+                 P1OUT = ~BIT0;
             }
     }
 
@@ -40,9 +37,9 @@ int main(void)
         P2OUT ^= BIT0;                      //Toggle one of the two LED at different rate
         __delay_cycles(100000);             //Adjust speed
         if(!(P1IN & BIT1)){
-                               swap = 1;
-                               P2OUT = ~BIT0;
-                    }
+               swap = 1;
+               P2OUT = ~BIT0;
+             }
         }
     }
 
