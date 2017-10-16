502d89b4d201104843622ec199c7e7529be6e322
rodriguem3 <31865089+rodriguem3@users.noreply.github.com>
2017-10-16 19:24:44 -0400

    Update main.c

--- Multiple Blink/MultipleBlink(G2553)/main.c
+++ Multiple Blink/MultipleBlink(G2553)/main.c
@@ -9,25 +9,17 @@ int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
 
-    // Configure GPIO
-    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
-    P1DIR |= BIT0;                          // Set P1.0 to output direction
+    P1OUT &= ~BIT0;                         // Clear P1.0 output
+    P1DIR |= BIT0;                          // Set P1.0 to output 
     P1OUT &= ~BIT6;                        // Clear P1.1 output latch
     P1DIR |= BIT6;                          // Set p1.1 output latch
-
-    P1DIR &= ~BIT1;
-    P1REN |= BIT1;
-    P1OUT |= BIT1;
-
-   // PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
 
     while(1)
     {
         P1OUT ^= BIT0;                      // Toggle LED
         P1OUT ^= BIT6;                      //Toggle LED
-        __delay_cycles(500000);             //Adjust speed
-        P1OUT ^= BIT6;                      //Toggle one of the two LED at different rate
-        __delay_cycles(500000);             //Adjust speed
+        __delay_cycles(300000);             //delay
+        P1OUT ^= BIT6;                      //Toggle LED different rate
+        __delay_cycles(300000);             /delay
     }
 }
