0f4a544b8c2fea606d72e45fa6679764cbd68cbe
rodriguem3 <31865089+rodriguem3@users.noreply.github.com>
2017-10-16 19:36:28 -0400

    Update main.c

--- Button Blink/ButtonBlink(F5529)/main.c
+++ Button Blink/ButtonBlink(F5529)/main.c
@@ -10,17 +10,14 @@ int main(void)
     WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
 
     // Configure GPIO
-    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
-    P1DIR |= BIT0;                          // Set P1.0 to output direction
-    P4OUT &= ~BIT7;                        // Clear P1.1 output latch
-    P4DIR |= BIT7;                          // Set p1.1 output latch
-
+    P1DIR |= BIT0;                          // Set P1.0 to output 
+    P1OUT &= ~BIT0;                         // Clear P1.0 output 
+    P4DIR |= BIT7;                          // Set p1.1 output 
+    P4OUT &= ~BIT7;                        // Clear P1.1
     P2DIR &= ~BIT1;
     P2REN |= BIT1;
     P2OUT |= BIT1;
 
-   // PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
     int swap = 1;
 
 
